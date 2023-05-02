de1465f3997f27fd7cbdbb2362cbcb0eca59da8d
seishinkouki <sgflgg@gmail.com>
2023-05-03 01:45:44 +0800

    optimization: 1. Update the view as soon as the device is added or removed 2. Integrate the binding function under administrator authority, no need to go to cli

--- UsbipdGuiDemo/ViewModels/RootViewModel.cs
+++ UsbipdGuiDemo/ViewModels/RootViewModel.cs
@@ -4,12 +4,14 @@ using System.Diagnostics;
 using System.Text;
 using System.Windows.Threading;
 using System;
-
 using MessageBox = HandyControl.Controls.MessageBox;
 using System.Threading.Tasks;
 using System.Linq;
 using HandyControl.Tools.Extension;
 using System.Reflection;
+using System.Management;
+using System.Security.Principal;
+using System.Windows;
 
 namespace UsbipdGuiDemo.ViewModels
 {
@@ -138,17 +140,43 @@ namespace UsbipdGuiDemo.ViewModels
             };
             detachProcess.Start();
         }
+        public void BindDevices(string _busid)
+        {
+            var BindProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "usbipd",
+                    Arguments = " bind --busid " + _busid,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true,
+                }
+            };
+            BindProcess.Start();
+            BindProcess.WaitForExit();
+            AttachDevices(_busid);
+        }
 
         public RootViewModel()
         {
-
-            ListDevices();
             DispatcherTimer timer = new DispatcherTimer();
             timer.Tick += DispatcherTimer_Tick;
             timer.Interval = TimeSpan.FromMilliseconds(3000);
             timer.Start();
+
+            ListDevices();
+            ManagementEventWatcher watcher = new ManagementEventWatcher();
+            WqlEventQuery query = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 2 OR EventType = 3");
+            watcher.EventArrived += new EventArrivedEventHandler(watcher_EventArrived);
+            watcher.Query = query;
+            watcher.Start();
         }
 
+        public  void watcher_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            Task.Factory.StartNew(AsynchronousRefresh);
+        }
         public void DispatcherTimer_Tick(object sender, EventArgs e)
         {
             Task.Factory.StartNew(AsynchronousRefresh);
@@ -173,11 +201,36 @@ namespace UsbipdGuiDemo.ViewModels
                     DetachDevices(vm.BUSID);
                     break;
                 case "Not shared":
-                    string messageBoxText = "you have to make device shared by cmdline first!";
-                    string caption = "device not shareable";
-                    MessageBox.Warning(messageBoxText, caption);
+                    if (!IsAdministrator())
+                    {
+                        MessageBoxResult userResult = MessageBox.Show("绑定新设备需要管理器权限, 是否以管理员权限重启应用", "提示", MessageBoxButton.YesNo);
+                        if (userResult == MessageBoxResult.Yes)
+                        {
+                            PerformRestart();
+                        }
+                    }
+                    else
+                    {
+                        Task task = new Task(() => BindDevices(vm.BUSID));
+                        task.Start();
+                    }
                     break;
             }
+        }
+        public static void PerformRestart()
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = Process.GetCurrentProcess().MainModule.FileName;
+            proc.StartInfo.UseShellExecute = true;
+            proc.StartInfo.Verb = "runas";
+            proc.Start();
+            Application.Current.Shutdown();
+        }
+        public static bool IsAdministrator()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
         }
     }
 }
